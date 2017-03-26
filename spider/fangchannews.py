33610f62c487e9e195e1092c0da1eabb2ca8bc1d
hedj <dongjuyunzhonghe@126.com>
2017-03-26 23:31:05 +0800

    fangchannews.py tile and uml

--- spider/fangchannews.py
+++ spider/fangchannews.py
@@ -10,10 +10,19 @@
 
 """
 import requests
+from bs4 import BeautifulSoup
+
 res = requests.get('http://fangchan.news.baidu.com')
 #res.encoding  = 'utf-8' ###用自己指定的utf-8编码格式解析原来站点真实的编码格式，如果原来站点的编码格式不是utf-8，则就会出现乱码
 ###解决乱码问题：
 #print(res.apparen_encoding) ###获得原站点的真实编码格式（GB2312）,指定用GB2312格式去解析原站点的数据
 res.encoding = 'GB2312'
-print(res.text)
+soup = BeautifulSoup(res.text,'html.parser')
+for news in soup.select('.tlc-fl'):
+    h2 = (news.select('a')[0].text)
+    myhref = news.select('a')[0]['href']
+#   print(h2,myhref) #打印有问题 
+    print(h2)
+    print(myhref)
+#print(res.text)
 
